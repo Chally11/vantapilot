8dc035ddb7884966c0795a8a86b1dc7d6e2e1604
chally113 <timchallenderphotos@gmail.com>
2026-04-16 09:20:32 -0400

    Update custom.py

--- selfdrive/ui/layouts/settings/custom.py
+++ selfdrive/ui/layouts/settings/custom.py
@@ -37,9 +37,6 @@ class CustomLayout(Widget):
       self._scan_button,
     ], line_separator=True, spacing=0)
 
-    # Toggles should be not available to change in onroad state
-    ui_state.add_offroad_transition_callback(self._update_toggles)
-
   def _render(self, rect):
     self._scroller.render(rect)
 
@@ -50,98 +47,3 @@ class CustomLayout(Widget):
 
   def _update_toggles(self):
     ui_state.update_params()
-
-    # Hide non-release toggles on release builds
-    # TODO: we can do an onroad cycle, but alpha long toggle requires a deinit function to re-enable radar and not fault
-    for item in (self._joystick_toggle, self._long_maneuver_toggle, self._lat_maneuver_toggle, self._alpha_long_toggle):
-      item.set_visible(not self._is_release)
-
-    # CP gating
-    if ui_state.CP is not None:
-      alpha_avail = ui_state.CP.alphaLongitudinalAvailable
-      if not alpha_avail or self._is_release:
-        self._alpha_long_toggle.set_visible(False)
-        self._params.remove("AlphaLongitudinalEnabled")
-      else:
-        self._alpha_long_toggle.set_visible(True)
-
-      long_man_enabled = ui_state.has_longitudinal_control and ui_state.is_offroad()
-      self._long_maneuver_toggle.action_item.set_enabled(long_man_enabled)
-      if not long_man_enabled:
-        self._long_maneuver_toggle.action_item.set_state(False)
-        self._params.put_bool("LongitudinalManeuverMode", False)
-
-      lat_man_enabled = ui_state.is_offroad()
-      self._lat_maneuver_toggle.action_item.set_enabled(lat_man_enabled)
-    else:
-      self._long_maneuver_toggle.action_item.set_enabled(False)
-      self._lat_maneuver_toggle.action_item.set_enabled(False)
-      self._alpha_long_toggle.set_visible(False)
-
-    # TODO: make a param control list item so we don't need to manage internal state as much here
-    # refresh toggles from params to mirror external changes
-    for key, item in (
-      ("AdbEnabled", self._adb_toggle),
-      ("SshEnabled", self._ssh_toggle),
-      ("JoystickDebugMode", self._joystick_toggle),
-      ("LongitudinalManeuverMode", self._long_maneuver_toggle),
-      ("LateralManeuverMode", self._lat_maneuver_toggle),
-      ("AlphaLongitudinalEnabled", self._alpha_long_toggle),
-      ("ShowDebugInfo", self._ui_debug_toggle),
-    ):
-      item.action_item.set_state(self._params.get_bool(key))
-
-  def _on_enable_ui_debug(self, state: bool):
-    self._params.put_bool("ShowDebugInfo", state)
-    gui_app.set_show_touches(state)
-    gui_app.set_show_fps(state)
-
-  def _on_enable_adb(self, state: bool):
-    self._params.put_bool("AdbEnabled", state)
-
-  def _on_enable_ssh(self, state: bool):
-    self._params.put_bool("SshEnabled", state)
-
-  def _on_joystick_debug_mode(self, state: bool):
-    self._params.put_bool("JoystickDebugMode", state)
-    self._params.put_bool("LongitudinalManeuverMode", False)
-    self._long_maneuver_toggle.action_item.set_state(False)
-    self._params.put_bool("LateralManeuverMode", False)
-    self._lat_maneuver_toggle.action_item.set_state(False)
-
-  def _on_long_maneuver_mode(self, state: bool):
-    self._params.put_bool("LongitudinalManeuverMode", state)
-    self._params.put_bool("JoystickDebugMode", False)
-    self._joystick_toggle.action_item.set_state(False)
-    self._params.put_bool("LateralManeuverMode", False)
-    self._lat_maneuver_toggle.action_item.set_state(False)
-
-  def _on_lat_maneuver_mode(self, state: bool):
-    self._params.put_bool("LateralManeuverMode", state)
-    self._params.put_bool("ExperimentalMode", False)
-    self._params.put_bool("JoystickDebugMode", False)
-    self._joystick_toggle.action_item.set_state(False)
-    self._params.put_bool("LongitudinalManeuverMode", False)
-    self._long_maneuver_toggle.action_item.set_state(False)
-
-  def _on_alpha_long_enabled(self, state: bool):
-    if state:
-      def confirm_callback(result: DialogResult):
-        if result == DialogResult.CONFIRM:
-          self._params.put_bool("AlphaLongitudinalEnabled", True)
-          self._params.put_bool("OnroadCycleRequested", True)
-          self._update_toggles()
-        else:
-          self._alpha_long_toggle.action_item.set_state(False)
-
-      # show confirmation dialog
-      content = (f"<h1>{self._alpha_long_toggle.title}</h1><br>" +
-                 f"<p>{self._alpha_long_toggle.description}</p>")
-
-      dlg = ConfirmDialog(content, tr("Enable"), rich=True, callback=confirm_callback)
-      gui_app.push_widget(dlg)
-
-    else:
-      self._params.put_bool("AlphaLongitudinalEnabled", False)
-      self._params.put_bool("OnroadCycleRequested", True)
-      self._update_toggles()
